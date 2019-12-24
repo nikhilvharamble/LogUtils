55b8e9a1cc1a25bdd10daff952a52e8968eea186
Nikhil Vharamble <nikhilvharamble@nciportal.com>
2019-12-24 13:44:54 +0530

    initail commit

---
+++ app/src/main/java/android_developers/nikssoft/com/logutils/MainActivity.java
@@ -0,0 +1,10 @@
+package android_developers.nikssoft.com.logutils;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ logutil/src/main/java/android_developers/nikssoft/com/logutil/Logs.java
@@ -0,0 +1,12 @@
+package android_developers.nikssoft.com.logutil;
+import android.util.Log;
+/**
+ * Created by developers on 24/12/19.
+ */
+public class Logs {
+
+    public static void d(String tag,String msg)
+    {
+        Log.d(tag,msg);
+    }
+}
